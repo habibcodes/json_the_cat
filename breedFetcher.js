a7bef388023517700dc9745cdb22ce2f6afd3a4e
habibcodes <habib.codes@gmail.com>
2021-09-23 16:06:32 -0400

    refactored to index file using callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,27 +3,36 @@
 // dependencies
 const request = require('request');
 
-// extract breed entered from cmd line
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breedss/search?q=${breed}`, (error, response, body) => {
-  // if 404
-  if (response.statusCode === 404) {
-    console.log(`Status Code: ${response && response.statusCode}, Request Failed.`); // Print the response status code if a response was received
-    return;
-  }
-  // parse api string array-obj
-  const parsedBreed = JSON.parse(body); // puts it into JSON object
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  // if breed not found, return err
-  if (parsedBreed[0].name !== breed) {
-    console.log(`Error: ${breed} not found as breed.`); // Print the error if one occurred
-    return;
-  }
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log(typeof body); // first determine data format
-  console.log(typeof parsedBreed); // now it's an obj structure
+    // console.log(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`);
 
-  // console.log(parsedBreed);
-  console.log(`The breed description for ${breed}: ${parsedBreed[0].description}`);
-});
+    if (error) {
+      callback(error.message, null);
+      return;
+    }
+
+    // #### ---> failure
+    // if 404
+    if (response.statusCode === 404) {
+      callback(`Status Code: ${response && response.statusCode}, Request Failed.`, null); // Print the response status code if a response was received
+      return;
+    }
+    // parse api string array-obj
+    const parsedBreed = JSON.parse(body); // puts it into JSON object
+
+    // if breed not found, return err
+    if (parsedBreed[0].name !== breedName) {
+      callback(`Error: ${breedName} not found as breed.`, null); // Print the error if one occurred
+      return;
+    }
+        
+    // ######## --> success
+    // callback (parsedBreed);
+    callback(null, `The breed description for ${breedName}: ${parsedBreed[0].description}`);
+  });
+};
+
+module.exports = fetchBreedDescription;
