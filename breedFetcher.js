f794ab7bc6ed61d8e9d975b32da3b785378a7ef6
habibcodes <habib.codes@gmail.com>
2021-09-23 15:00:08 -0400

    implemented breedFetcher API call and JSON parsing!!

---
+++ breedFetcher.js
@@ -0,0 +1,26 @@
+// Implement breedFetcher //
+
+// dependencies
+const request = require('request');
+
+// extract breed entered from cmd line
+const breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breedss/search?q=${breed}`, (error, response, body) => {
+  if (response.statusCode === 404) {
+    console.log(`Status Code: ${response && response.statusCode}, Request Failed.`); // Print the response status code if a response was received
+    return;
+  }
+  const parsedBreed = JSON.parse(body); // puts it into JSON object
+
+  if (parsedBreed[0].name !== breed) {
+    console.log(`Error: ${breed} not found as breed.`); // Print the error if one occurred
+    return;
+  }
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log(typeof body); // first determine data format
+  console.log(typeof parsedBreed); // now it's an obj structure
+
+  // console.log(parsedBreed);
+  console.log(`The breed description for ${breed}: ${parsedBreed[0].description}`);
+});
